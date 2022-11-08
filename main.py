2f64b65fbe84a1a50327958b58127560fcf492e2
OPuusalu <91115628+OPuusalu@users.noreply.github.com>
2022-11-08 20:02:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ import random
 import re
 from discord.ext import tasks
 
-
 client = discord.Client(intents=discord.Intents.all())
 ch = None
 
+importantList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u'
+    , 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+specialWords = ['cunt', 'stop', 'start']
+
+list2 = 'qwertyuiopasdfghjklzxcvbnmüõö ä1234567890!@#$%^&*()[]{};:'"<>,./?\|"
 
 def get_image():
     link = "http://catgirldatabase.com/picture.php?/{}/category/1".format(random.randint(0, 4260))
     a = requests.get(link).text
     soup = BeautifulSoup(a, 'html.parser')
 
-    #ToDo if madis watches too many cat images tere might be index out of range error for some reason
+    # ToDo if madis watches too many cat images tere might be index out of range error for some reason
     text = str(soup.find_all('img')[0])
     s = re.search('src="(.*)" title', text).group(1)
 
@@ -32,9 +37,10 @@ def get_image():
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
-    global ch
+    global ch, loopTime
     if message.author == client.user:
         return
 
@@ -42,27 +48,38 @@ async def on_message(message):
         await message.channel.send("Hello master " + message.author.nick)
 
     if message.content == "modis" or message.content == '@Modis':
-        await message.channel.send('https://media.discordapp.net/attachments/1033478386196156446/1033495265367306330/madis_hmm_gif.gif')
+        await message.channel.send(
+            'https://media.discordapp.net/attachments/1033478386196156446/1033495265367306330/madis_hmm_gif.gif')
 
     if message.content == "start":
-
         ch = message.channel
         catgirl.start()
 
-    if message.content == "stop" or message.content == "This nonsense needs to end!":
+    if message.content == "stop":
+        ch = message.channel
+        await message.channel.send("LMAO *sToP*, there is no end to modis!")
+
+    if message.content == "This nonsense needs to end!":
         ch = message.channel
         catgirl.cancel()
         await message.channel.send("The catgirls have been imprisoned (end)")
 
-@tasks.loop(seconds = 5)
+    if message.content[int(len(message.content)/2)].lower() in list2 and not message.content in specialWords:
+        ch = message.channel
+        await message.channel.send('OK :thumbsup:')
+
+    if specialWords[0] in message.content:
+        ch = message.channel
+        await message.channel.send('??? :thumbsdown:')
+
+
+@tasks.loop(seconds=5)
 async def catgirl():
-    randomNumber = 0 #random.randint(0, 10) while not in tartu ylikool
+    randomNumber = 0  # random.randint(0, 10) while not in tartu ylikool
     if randomNumber == 0:
-        await ch.send(
-            'https://media.discordapp.net/attachments/1033478386196156446/1033495265367306330/madis_hmm_gif.gif')
+        await ch.send('https://media.discordapp.net/attachments/1033478386196156446/1033495265367306330/madis_hmm_gif.gif')
     else:
         await ch.send(embed=get_image())
-
 
 
 client.run("MzUxMzM2MTk0OTY3MDc2ODY0.GdvZD6.EeU8chS1boc6_CtZyrweXua__K1c_iC6Am5FuE")
